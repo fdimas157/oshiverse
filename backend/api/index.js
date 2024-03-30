c1f4f6dd65077d2ace1b2216d38325f1daa1f209
Dimas Firmansyah <fdimas157@gmail.com>
2024-03-30 23:04:23 +0700

    feat: add first endpoint

---
+++ backend/api/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { pool } from "./db.js";
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server berjalan dengan baik!");
+});
+
+app.get("/api", (_req, res) => {
+  res.send("Hello world!");
+});
+
+app.get("/api/v1/users", async (_req, res) => {
+  const result = await pool.query("SELECT * FROM users");
+  res.json(result.rows);
+});
